dcf75469b5b0d6bdca1c54bdd459437cd9e4c9aa
Marcus Edralin <medralin36@yahoo.com>
2021-04-12 15:09:17 -0700

    Fundamentals Assignment & Notes

---
+++ assignments.js
@@ -0,0 +1,5 @@
+let country = 'USA';
+let continent = 'North America';
+let population = '328 Million';
+
+console.log(country, continent, population);
--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 let js = 'amazing';
-      if(js === 'amazing') {
-        alert('JavaScript is HARD')
-      };
+console.log(32+43+222-389);
 
-      54+32+3-10;
-      console.log(54+32+3-10);
+console.log('Marcus');
+// just reviewing values
+// nice to get back to the basics to master React
+// you can store values in variables to use them over and over again
+
+let firstName = 'Marcus';
+let first = 'See';
+let firstNameDog = 'Skywalker';
+let first_name_person = 'bobby';
+// this is declaring a variable
+// it stores that value inside that variable
+console.log(firstName);
+// instead of passing a literal value we call the variable name
+console.log(firstName);
+console.log(firstName);
+// you can change a variable name once and it will change in all places used
+// without variables you would have to change it everywhere
+
+// camelCase is a naming convention
+
+//let 3years = 3; this is an invalid or unexpected error token due to the name of the variable
+// marcus&see = 'ms'; this also doesn't work due to the & sign
+// let new = 27; this doesn't work because new is a reserved key word
+// let function = 56; this will not work but $function will
+// let Person = 'Steven'; another naming convention-all names should start lower case
+let PI = 3.135; 
+
+// REMEMBER TO WRITE YOUR VARIABLE NAMES IN A DESCRIPTIVE WAY
+let myFirstJob = 'Recreational Aide';
+let myCurrentJob = 'Developer';
+// the above example makes more descriptive
+let job1 = 'rec aide';
+let job2 = 'developer';
+
+console.log(myCurrentJob);
