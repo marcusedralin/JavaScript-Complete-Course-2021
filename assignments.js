468a8619d2dcfc15edcba3452a09b2348e330ccf
Marcus Edralin <medralin36@yahoo.com>
2021-04-12 18:43:51 -0700

    Basic Operators

--- assignments.js
+++ assignments.js
@@ -1,20 +1,41 @@
 const country = 'USA';
-// users country shouldn't change
+// // users country shouldn't change
 const continent = 'North America';
-// users continent shouldn't mutate
-let population = '328 Million';
-// number can change as people birthed/die
+// // users continent shouldn't mutate
+let usaPop = 328000000;
+// // number can change as people birthed/die
+usaPop++;
+console.log(usaPop); // 328000001
 
-// console.log(country, continent, population);
+const finlandPop = 6000000;
 
-const isIsland = false;
-// const because we wouldn't want a boolean to change in this situation
+if(usaPop > finlandPop) {
+    console.log('USA has larger population');
+} else {
+    console.log('Finland has a larger population');
+};
+
+const averagePop = 33000000;
+
+if(usaPop > averagePop) {
+    console.log('USA has an above average population');
+} else {
+    console.log('USA has a below average population');
+}
+
+
+// // console.log(country, continent, population);
+
+// const isIsland = false;
+// // const because we wouldn't want a boolean to change in this situation
 let language = 'English';
 
-language = 'Spanish';
-// isIsland = true; // doesn't work because we are mutating a const
+console.log(`${country} is in ${continent}, and its ${usaPop} people speak ${language}`);
 
-console.log(typeof isIsland); //boolean
-console.log(typeof language); //undefined
+// language = 'Spanish';
+// // isIsland = true; // doesn't work because we are mutating a const
+
+// console.log(typeof isIsland); //boolean
+// console.log(typeof language); //undefined
 
 
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+////// Intro ///////
+
 // let js = 'amazing';
 // console.log(32+43+222-389);
 
@@ -52,6 +54,8 @@
 // console.log(typeof javascriptIsFun);
 // // the value changed but the previous value/typeof still returns as true/boolean;
 
+///// Data Types //////
+
 // let year;
 // console.log(year);
 // console.log(typeof year);
@@ -65,25 +69,64 @@
 // // typeof is object
 // // error. null is not an object.
 
-let age = 30;
-age = 31;
-// with let you can reassign/mutate a value to the variable
+////// Var, Let and Const //////
 
-const birthYear = 1990;
-// birthYear = 1990; 
-// error/immutable 
-// const dog;
-// error missing initalizer
+// let age = 30;
+// age = 31;
+// // with let you can reassign/mutate a value to the variable
 
-// Always use const by default to help from causing bugs do unwanted mutation unless you know you variable is supposed to change.
-// var shouldn't be used
+// const birthYear = 1990;
+// // birthYear = 1990; 
+// // error/immutable 
+// // const dog;
+// // error missing initalizer
 
-var job = 'programmer';
-job = 'teacher';
+// // Always use const by default to help from causing bugs do unwanted mutation unless you know you variable is supposed to change.
+// // var shouldn't be used
 
-lastName = 'Edralin';
-console.log(lastName);
-// this works but JS creates a property on the global object by not declaring the variable
+// var job = 'programmer';
+// job = 'teacher';
+
+// lastName = 'Edralin';
+// console.log(lastName);
+// // this works but JS creates a property on the global object by not declaring the variable
+
+// Operators //
+
+// Math operators
+const now = 2021;
+const ageMarcus = now - 1990;
+const ageSee = now - 1994;
+console.log(ageMarcus, ageSee);
+
+console.log(ageMarcus * 2, ageMarcus / 10, 2 ** 3);
+// 2 ** 3 means two to the power of three 3 = 2 * 2 * 2
+
+const firstName = 'Marcus';
+const lastName = 'Edralin';
+console.log(firstName + ' ' + lastName);
+// concatenation
+
+// Assignment Operators
+let x = 23 + 3; // 26
+x += 12; // x = x + 12 = 38
+x *= 2; // x = x * 2 = 76
+x++; // x = x + 1 = 77
+x--; // x = x - 1 = 76
+console.log(x);
+
+// Comparison Operators
+console.log(ageMarcus > ageSee); // true < > <= >=
+console.log(ageSee >= 18); // true 
+
+const isFullAge = ageSee >= 18;
+
+console.log(now - 1990 > now - 1994);
+// JS doesn't work left to right
+// handles the math operations then the comparison operation
+
+
+
 
 
 
